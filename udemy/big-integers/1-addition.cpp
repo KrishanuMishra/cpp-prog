c2c60e59b5d374374d3c9667aa75669ae2ade4de
Krishanu Mishra <krishanumishra@Krishanus-MacBook-Air.local>
2023-12-25 22:13:41 +0530

    Big Addition

--- udemy/big-integers/1-addition.cpp
+++ udemy/big-integers/1-addition.cpp
@@ -1,59 +1,35 @@
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-char digitToChar(int digit){
-	return digit + '0';
+int charToint(char c){
+	return c -'0';
 }
-
-int charToDigit(char ch){
-	return ch - '0';
+int intToChar(char c){
+	return c +'0';
 }
-
-string addNumbers(string n1, string n2){
-	if(n1.length() > n2.length()){
-		swap(n1,n2);
-	}
-	string result = "";
-	reverse(n1.begin(),n1.end());
-	reverse(n2.begin(),n2.end());
-	int carry = 0;
-	for(int i=0; i < n1.length(); i++){
-		int d1 = charToDigit(n1[i]);
-		int d2 = charToDigit(n2[i]);
-		int sum = d1 + d2 + carry;
-		int output_digit = sum % 10;
-		carry = sum/10;
-		result.push_back(digitToChar(output_digit));
-	}
-
-	for(int i = n1.length(); i < n2.length(); i++){
-		int d2 = charToDigit(n2[i]);
-		int sum = d2 + carry;
-		int output_digit = sum % 10;
-		carry = sum/10;
-		result.push_back(digitToChar(output_digit));
-	}
-	if(carry)
-	{
-		result.push_back('1');
-	}
-	reverse(result.begin(), result.end());
-	return result;
-}
-
-
 
 int main(){
 	string s1,s2;
-	cin>> s1 >> s2;
-
-	string result = addNumbers(s1,s2);
-	cout << result <<endl;
-
-	int a,b;
-	cin>>a>>b;
-	cout << a + b <<endl;
-    
-	return 0;
+	cin>>s1>>s2;
+	string ans = "";
+	reverse(s1.begin(),s1.end());
+	reverse(s2.begin(),s2.end());
+	int carry = 0;
+	int minlength = min(s1.length(),s2.length());
+	int maxlength = max(s1.length(),s2.length());
+	for(int i =0;i<minlength;i++){
+		int a = charToint(s1[i])+charToint(s2[i]);
+		ans += intToChar(carry + a%10);
+		carry = a/10;
+	}
+	for(int i=minlength;i<maxlength;i++){
+		if(s1.length()>s2.length()){
+		ans+= intToChar(carry+charToint(s1[i]));
+		}else{
+		ans+= intToChar(carry+charToint(s2[i]));
+		}
+		carry = 0;
+	}
+	reverse(ans.begin(),ans.end());
+	cout<<ans<<endl;
 }
